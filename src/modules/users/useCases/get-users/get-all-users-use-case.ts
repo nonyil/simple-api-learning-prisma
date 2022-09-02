9e71ba920940ffbe77e71cbc15678bb687210165
nonyil <igbrendow@gmail.com>
2022-09-02 18:26:36 -0300

    feat: configs in get-all-users / Finished

--- src/modules/users/useCases/get-users/get-all-users-use-case.ts
+++ src/modules/users/useCases/get-users/get-all-users-use-case.ts
@@ -4,7 +4,19 @@ import prisma from "../../../../prisma/client";
 
 export class GetAllUsersUseCase {
   async execute(): Promise<User[]> {
-    const users = await prisma.user.findMany({});
+    const users = await prisma.user.findMany({
+      include: {
+        movie_rents: {
+          select: {
+            movie: {
+              select: {
+                title: true,
+              }
+            }
+          }
+        }
+      }
+    });
 
     return users;
   }
